ffbf4d18c716ef525e45c242244c66dd05eb8e45
jvadebossan <jvdebosssan@gmail.com>
2024-06-03 23:43:23 -0300

    change from json to localstorage

--- scripts/load_ongs.js
+++ scripts/load_ongs.js
@@ -1,25 +1,28 @@
-function onLoad(){
+function onLoad() {
     let divOngs = document.getElementsByClassName('ongs')[0]
 
-    fetch('../database.json')
-        .then(response => response.json())
-        .then(data => {
-            const ongs = JSON.stringify(data)
-            data = JSON.parse(ongs).ongs
-            data.forEach(ong => {
-                console.log(ong)
-            let div = document.createElement('div')
-            div.className = 'ong'
+
+    const storedData = localStorage.getItem('ongs');
+    if (storedData) {
+        const data = JSON.parse(storedData);
+        data.forEach(ong => {
+            let div = document.createElement('div');
+            div.className = 'ong';
             div.innerHTML = `
-                <h1>${ong.name}</h1>
-                <h2>${ong.desc}</h2>
-                <h3>${ong.cnpj}</h3>
-                <input class="btn" type="button" value="DELETAR">
-                <input class="btn" type="button" value="EDITAR">
-            `
-            divOngs.appendChild(div)
-            })
-        })
+            <h1>${ong.name}</h1>
+            <h2>${ong.desc}</h2>
+            <h3>${ong.cnpj}</h3>
+            <input class="btn" type="button" value="DELETAR">
+            <input class="btn" type="button" value="EDITAR">
+        `;
+            divOngs.appendChild(div);
+        });
+    } else {
+        let errorMsg = document.createElement('p')
+        errorMsg.innerText = 'Nenhuma ONG cadastrada!';
+        errorMsg.className = 'error';
+        divOngs.appendChild(errorMsg);
+    }
 }
 
 onLoad()
